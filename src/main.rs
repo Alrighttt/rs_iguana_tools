21d92b6066de815e96b1fb8e81f20d2ee2ad721d
Alright <mzlot555@gmail.com>
2023-07-04 22:47:24 -0400

    extremely basic threading behavior & RPC server

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use nanomsg::{Protocol, Socket};
 
 use std::env;
 use std::io::Read;
+use std::thread;
 
 use iguana_rs::{
     validate_packet_signature, validate_packethash, DpowNanoMsgHdr, IguanaPacketHeader, FIRST_PARTY,
@@ -11,6 +12,16 @@ use iguana_rs::{
 // TODO: cleanup all db OPs into other file
 use iguana_rs::db::{init_db, update_ip_logs, update_known_ips, update_lastseen};
 use rusqlite::Connection;
+
+use jsonrpc_core::types::error::Error;
+use jsonrpc_core::types::params::Params;
+use jsonrpc_core::types::Value;
+use jsonrpc_core::IoHandler;
+use jsonrpc_http_server::ServerBuilder;
+//use jsonrpc_http_server::*;
+use std::sync::{Arc, Mutex};
+
+use futures::future;
 
 fn print_hex(bytes: &[u8]) {
     for b in bytes {
@@ -35,19 +46,44 @@ fn reverse_bits(byte: u8) -> u8 {
     r
 }
 
+fn printinfo(dpow_msg: &DpowNanoMsgHdr) {
+    println!(
+        "{} {:?} {} {} channel:{:?} bestk:{}",
+        FIRST_PARTY[dpow_msg.senderind as usize],
+        dpow_msg.myipbits,
+        std::str::from_utf8(&dpow_msg.symbol).unwrap(),
+        dpow_msg.height,
+        dpow_msg.channel,
+        dpow_msg.notarize.bestk,
+    );
+    print!("bestmask:");
+    for byte in &dpow_msg.notarize.bestmask {
+        print!("{:08b}", reverse_bits(*byte));
+    }
+    println!();
+    print!("recvmask:");
+    for byte in &dpow_msg.notarize.recvmask {
+        print!("{:08b}", reverse_bits(*byte));
+    }
+    println!();
+    print!("srchash:");
+    print_hex(&dpow_msg.srchash);
+    print!("desthash:");
+    print_hex(&dpow_msg.desthash);
+}
+
 // usage ./iguana_rs_listener <external IP to bind to> <port to bind to> <initial peer to connect to> <db filename>
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let server_ip = &args[1];
-    let server_port = &args[2];
+    let server_ip = args[1].clone();
+    let server_port = args[2].clone();
     let server_url = format!("tcp://{}:{}", server_ip, server_port);
 
-    let bootstrap_peer = &args[3];
+    let bootstrap_peer = args[3].clone();
     let peer_string = format!("tcp://{}:{}", bootstrap_peer, server_port);
 
-    let conn = Connection::open(&args[4]).unwrap();
-    init_db(&conn);
+    let db_file = args[4].clone();
 
     let binconf = bincode::DefaultOptions::new().with_fixint_encoding();
 
@@ -60,86 +96,102 @@ fn main() {
     };
 
     let mut buffer = vec![];
-    let mut connect_once = true;
+    //let mut connect_once = true;
+    let connect_once = Arc::new(Mutex::new(true));
+    let connect_once_for_thread = connect_once.clone();
+
     //let mut out_sockets : Vec<Socket> = Vec::new();
-    loop {
-        match in_socket.read_to_end(&mut buffer) {
-            Ok(_mysize) => {
-                loop {
-                    if buffer.len() == 0 {
-                        break;
-                    };
+    thread::spawn(move || {
+        let conn = Connection::open(db_file).unwrap();
+        init_db(&conn);
+        loop {
+            match in_socket.read_to_end(&mut buffer) {
+                Ok(_mysize) => {
+                    loop {
+                        if buffer.len() == 0 {
+                            break;
+                        };
 
-                    let header: IguanaPacketHeader = binconf.deserialize(&buffer[..104]).unwrap();
-                    buffer = buffer[104..].to_vec();
+                        println!("full buffer {:?}", buffer);
+                        let header: IguanaPacketHeader =
+                            binconf.deserialize(&buffer[..104]).unwrap();
+                        buffer = buffer[104..].to_vec();
 
-                    validate_packethash(&header, &buffer).unwrap();
-                    let _pubkey = validate_packet_signature(&header).unwrap();
-                    // TODO: add "validate_pubkey" flag
+                        validate_packethash(&header, &buffer).unwrap();
+                        let _pubkey = validate_packet_signature(&header).unwrap();
+                        // TODO: add "validate_pubkey" flag
 
-                    let msg_size = std::mem::size_of::<DpowNanoMsgHdr>() - 1;
-                    let dpow_msg: DpowNanoMsgHdr =
-                        binconf.deserialize(&buffer[..msg_size]).unwrap();
-                    buffer = buffer[msg_size..].to_vec();
+                        let msg_size = std::mem::size_of::<DpowNanoMsgHdr>() - 1;
+                        let dpow_msg: DpowNanoMsgHdr =
+                            binconf.deserialize(&buffer[..msg_size]).unwrap();
+                        buffer = buffer[msg_size..].to_vec();
 
-                    update_lastseen(&conn, dpow_msg.senderind);
-                    update_ip_logs(&conn, dpow_msg.senderind, dpow_msg.myipbits);
-                    update_known_ips(&conn, dpow_msg.senderind, dpow_msg.ipbits.to_vec());
+                        update_lastseen(&conn, dpow_msg.senderind);
+                        update_ip_logs(&conn, dpow_msg.senderind, dpow_msg.myipbits);
+                        update_known_ips(&conn, dpow_msg.senderind, dpow_msg.ipbits.to_vec());
 
-                    if connect_once {
-                        for ip in dpow_msg.ipbits.iter() {
-                            let ip_str = Ipv4Addr::from(u32::from_be_bytes(*ip)).to_string();
-                            println!("ip_str {}", ip_str);
-                            if ip != &[0; 4] && server_ip != &ip_str && bootstrap_peer != &ip_str {
-                                let dial = format!("tcp://{}:{}", ip_str, server_port);
-                                println!("dial {}", dial);
-                                // TODO we want thread per connection to be able to send/receive selectively
-                                //let mut out_socket = Socket::new(Protocol::Bus).expect("cannot create socket");
-                                let _out_endpoint = match in_socket.connect(&dial) {
-                                    Ok(_) => {
-                                        println!("connect to {}", dial);
-                                        //out_sockets.push(out_socket);
-                                    }
-                                    Err(_) => println!("failed connect to {}", dial),
-                                };
+                        if *connect_once_for_thread.lock().unwrap() {
+                            println!("TRIGGER RPC");
+                            for ip in dpow_msg.ipbits.iter() {
+                                let ip_str = Ipv4Addr::from(u32::from_be_bytes(*ip)).to_string();
+                                if ip != &[0; 4]
+                                    && &server_ip != &ip_str
+                                    && &bootstrap_peer != &ip_str
+                                {
+                                    let dial = format!("tcp://{}:{}", ip_str, server_port);
+                                    println!("dial {}", dial);
+                                    // TODO we want thread per connection to be able to send/receive selectively
+                                    //let mut out_socket = Socket::new(Protocol::Bus).expect("cannot create socket");
+                                    let _out_endpoint = match in_socket.connect(&dial) {
+                                        Ok(_) => {
+                                            println!("connect to {}", dial);
+                                            //out_sockets.push(out_socket);
+                                        }
+                                        Err(_) => println!("failed connect to {}", dial),
+                                    };
+                                }
                             }
+                            *connect_once_for_thread.lock().unwrap() = false;
                         }
-                        connect_once = false;
-                    }
 
-                    let _extra = &buffer[..dpow_msg.datalen as usize];
+                        let _extra = &buffer[..dpow_msg.datalen as usize];
 
-                    println!(
-                        "{} {:?} {} {} channel:{:?} bestk:{}",
-                        FIRST_PARTY[dpow_msg.senderind as usize],
-                        dpow_msg.myipbits,
-                        std::str::from_utf8(&dpow_msg.symbol).unwrap(),
-                        dpow_msg.height,
-                        dpow_msg.channel,
-                        dpow_msg.notarize.bestk,
-                    );
-                    print!("bestmask:");
-                    for byte in &dpow_msg.notarize.bestmask {
-                        print!("{:08b}", reverse_bits(*byte));
+                        buffer = buffer[dpow_msg.datalen as usize..].to_vec();
                     }
-                    println!();
-                    print!("recvmask:");
-                    for byte in &dpow_msg.notarize.recvmask {
-                        print!("{:08b}", reverse_bits(*byte));
-                    }
-                    println!();
-                    print!("srchash:");
-                    print_hex(&dpow_msg.srchash);
-                    print!("desthash:");
-                    print_hex(&dpow_msg.desthash);
-
-                    buffer = buffer[dpow_msg.datalen as usize..].to_vec();
+                }
+                Err(err) => {
+                    println!("Client failed to receive msg '{}'.", err);
+                    break;
                 }
             }
-            Err(err) => {
-                println!("Client failed to receive msg '{}'.", err);
-                break;
-            }
         }
-    }
+    });
+
+    thread::spawn(move || {
+        let mut io = IoHandler::default();
+        let connect_once_rpc = connect_once.clone(); // We clone here because the closure requires ownership
+        io.add_method("set_connect_once", move |params: Params| {
+            future::ready(match params {
+                Params::Map(map) => {
+                    if let Some(Value::Bool(val)) = map.get("value") {
+                        *connect_once_rpc.lock().unwrap() = *val;
+                        Ok(Value::Bool(true))
+                    } else {
+                        Err(Error::invalid_params("Missing 'value'"))
+                    }
+                }
+                _ => Err(Error::invalid_params("Expected map")),
+            })
+        });
+
+        let server = ServerBuilder::new(io)
+            .threads(3)
+            .start_http(&"127.0.0.1:3030".parse().unwrap())
+            .unwrap();
+
+        println!("JSON-RPC server listening on 127.0.0.1:3030");
+        server.wait();
+    });
+
+    loop {}
 }
